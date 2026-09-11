d0b66067dbcca4e93f7a0ed75b8c93036397bd8f
raxgamer64-boop <raxgamer64@gmail.com>
2026-09-11 21:29:16 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
 material.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
